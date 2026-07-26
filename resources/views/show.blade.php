f09ed71fc07292cd2b42899ec77c9e7564747cda
Trionfovalentino <trionfovalentino26@gmail.com>
2026-07-26 13:42:46 +0200

    Update show.blade.php

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ $article['title'] }}</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     @include('navbar')
@@ -15,5 +16,6 @@
     <div style="margin-top: 20px;">
         <p>{{ $article['content'] }}</p>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
