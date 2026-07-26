1b49bc17c3e00d950d9b0f569fa01b466ba322ad
Trionfovalentino <trionfovalentino26@gmail.com>
2026-07-26 13:42:12 +0200

    Include Bootstrap for improved styling
    Added Bootstrap CSS and JS links for styling.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Blog con Controller - Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     @include('navbar')
@@ -10,5 +11,7 @@
     <h1>Benvenuto sul Blog Tech & Gaming!</h1>
     <p>Questo progetto utilizza i <strong>Controller</strong> in Laravel per separare la logica dalle rotte.</p>
     <p><a href="{{ route('articles.index') }}">Sfoglia tutti gli articoli &rarr;</a></p>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
