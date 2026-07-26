21738e072048bae43802ec405f3dccc59f5d77aa
Trionfovalentino <trionfovalentino26@gmail.com>
2026-07-26 13:43:11 +0200

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Tutti gli Articoli</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     @include('navbar')
@@ -20,5 +21,6 @@
             <br>
         @endforeach
     </ul>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
